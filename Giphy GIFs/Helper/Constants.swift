67efa2c217b7d04046f37714004a686be93eee14
Haresh Ghatala <haresh.ghatala@gmail.com>
2021-08-27 16:54:15 +0530

    Added constants & loader classes

---
+++ Giphy GIFs/Helper/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  Giphy GIFs
+//
+//  Created by Haresh Ghatala on 2021/08/24.
+//  Copyright © 2021 Haresh Ghatala. All rights reserved.
+//
+
+import Foundation
+
+public let giphyBaseURLString = "https://api.giphy.com/v1/gifs"
+public let giphyApiKey = "EGTwoAfkjDsl448QcBzzLdOvDXuVTdvH"
+public let apiDataLimit = 30
+
+public let gifFeedCellHeight = 200.0
+public let columnSpace = 15
+public let defaultCellWidth = 120
+
+public let gifFeedCellId = "GIFFeedTableViewCell"
+public let favouriteCellId = "FavouriteCollectionViewCell"
+
+public let infoMsgNoGifFound = "No results found."
+
+public let errorMsgInvalidEndpoint = "Invalid URL or Endpoint. Please retry."
+public let errorMsgInvalidResponse = "Invalid response or Data issue. Please retry."
+public let errorMsgUnknown = "Unknown Error. Please retry."
+
