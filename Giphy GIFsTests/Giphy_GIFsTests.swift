280753984470ef3335d2ee62db69e07a18d3d145
Haresh Ghatala <haresh.ghatala@gmail.com>
2021-08-27 16:50:30 +0530

    Initial project setup

---
+++ Giphy GIFsTests/Giphy_GIFsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Giphy_GIFsTests.swift
+//  Giphy GIFsTests
+//
+//  Created by Haresh Ghatala on 2021/08/24.
+//  Copyright © 2021 Haresh Ghatala. All rights reserved.
+//
+
+import XCTest
+@testable import Giphy_GIFs
+
+class Giphy_GIFsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
